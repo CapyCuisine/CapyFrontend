865784797b51a3e11fcc2a969273b628cbdd9c4d
IceK37 <45316097+IceK37@users.noreply.github.com>
2024-01-23 16:25:20 +0100

    hot fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/Home.css';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
 
 const lastRecipes = [
